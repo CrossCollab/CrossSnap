41054d0bfa9f2f981bb2d8b84d13a4b30819840c
Murad Zalov <mzalov@gmail.com>
2020-01-31 15:04:11 -0600

    Addded prettier config options inside package.json file

--- screens/CrosswordScreen.js
+++ screens/CrosswordScreen.js
@@ -1,167 +1,165 @@
-import * as WebBrowser from 'expo-web-browser'
-import React from 'react'
+import * as WebBrowser from "expo-web-browser";
+import React from "react";
 import {
-    Image,
-    Platform,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-    TextInput,
-} from 'react-native'
-import axios from 'axios'
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput
+} from "react-native";
+import axios from "axios";
 
-const SERVER_URL = 'http://' + '172.17.21.173:8080'
+const SERVER_URL = "http://" + "172.17.21.173:8080";
 // import * as Sharing from "expo-sharing";
 
 // import logo from "../assets/images/logo.png"; //need the png
 
-import { MonoText } from '../components/StyledText'
-import io from 'socket.io-client'
+import { MonoText } from "../components/StyledText";
+import io from "socket.io-client";
 export default class CrosswordTable extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            answers: [],
-            guesses: [],
-            isReady: false,
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      guesses: [],
+      isReady: false
+    };
+  }
+  async componentDidMount() {
+    try {
+      this._isMounted = true;
+      const { data } = await axios.get(`${SERVER_URL}/api/gameInstance/`);
+      this.setState({
+        answers: data.answers,
+        guesses: data.guesses,
+        isReady: true
+      });
+      this.socket = io(SERVER_URL);
+      this.socket.on("change puzzle", state => {
+        const { guesses } = state;
+        this.setState({ guesses });
+      });
+    } catch (err) {
+      console.log(err);
     }
-    async componentDidMount() {
-        try {
-            this._isMounted = true
-            const { data } = await axios.get(`${SERVER_URL}/api/gameInstance/`)
-            this.setState({
-                answers: data.answers,
-                guesses: data.guesses,
-                isReady: true,
-            })
-            this.socket = io(SERVER_URL)
-            this.socket.on('change puzzle', state => {
-                const { guesses } = state
-                this.setState({ guesses })
-            })
-        } catch (err) {
-            console.log(err)
-        }
-    }
-    handleChange = idx => letter => {
-        const allGuesses = JSON.parse(JSON.stringify(this.state.guesses))
-        allGuesses[idx].guess = letter
-        this.setState({ guesses: allGuesses }, this.changeHelper)
-    }
-    changeHelper() {
-        this.socket.emit('change puzzle', this.state)
+  }
+  handleChange = idx => letter => {
+    const allGuesses = JSON.parse(JSON.stringify(this.state.guesses));
+    allGuesses[idx].guess = letter;
+    this.setState({ guesses: allGuesses }, this.changeHelper);
+  };
+  changeHelper() {
+    this.socket.emit("change puzzle", this.state);
+  }
+
+  render() {
+    if (this.state && !this.state.isReady) {
+      return <Text>Loading...</Text>;
+    } else if (!this.state) {
+      return <Text>No state</Text>;
     }
 
-    render() {
-        if (this.state && !this.state.isReady) {
-            return <Text>Loading...</Text>
-        } else if (!this.state) {
-            return <Text>No state</Text>
-        }
-
-        let numOfRows = Math.sqrt(this.state.guesses.length)
-        let rows = []
-        for (let i = 0; i < numOfRows; i++) {
-            rows.push([])
-        }
-        for (let i = 0; i < this.state.guesses.length; i++) {
-            let currentRow = Math.floor(i / numOfRows)
-            let currentAnswer = this.state.guesses[i]
-            rows[currentRow].push(currentAnswer)
-        }
-        return (
-            //   <CrosswordTable rows={rows} guesses={this.state.guesses} answers={this.state.answers}
+    let numOfRows = Math.sqrt(this.state.guesses.length);
+    let rows = [];
+    for (let i = 0; i < numOfRows; i++) {
+      rows.push([]);
+    }
+    for (let i = 0; i < this.state.guesses.length; i++) {
+      let currentRow = Math.floor(i / numOfRows);
+      let currentAnswer = this.state.guesses[i];
+      rows[currentRow].push(currentAnswer);
+    }
+    return (
+      //   <CrosswordTable rows={rows} guesses={this.state.guesses} answers={this.state.answers}
+      <View
+        style={{
+          flexDirection: "column",
+          flex: 1,
+          alignContent: "flex-start",
+          justifyContent: "flex-start",
+          height: "80%",
+          marginTop: "10%"
+        }}
+      >
+        {rows.map((row, index) => {
+          return (
             <View
-                style={{
-                    flexDirection: 'column',
-                    flex: 1,
-                    alignContent: 'flex-start',
-                    justifyContent: 'flex-start',
-                    height: '80%',
-                    marginTop: '10%',
-                }}
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                borderColor: "black",
+                borderWidth: 2,
+                height: `${100 / numOfRows}%`
+              }}
+              key={index}
             >
-                {rows.map((row, index) => {
-                    return (
-                        <View
-                            style={{
-                                flex: 1,
-                                flexDirection: 'row',
-                                borderColor: 'black',
-                                borderWidth: 2,
-                                height: `${100 / numOfRows}%`,
-                            }}
-                            key={index}
-                        >
-                            {row.map((cell, idx) => {
-                                if (cell.answer === '.') {
-                                    return (
-                                        <TextInput
-                                            maxLength={0}
-                                            style={{
-                                                backgroundColor: 'black',
-                                                height: '100%',
-                                                width: `${100 / numOfRows}%`,
-                                                borderColor: 'black',
-                                                borderWidth: 1,
-                                                justifyContent: 'center',
-                                            }}
-                                            textAlign={'center'}
-                                            key={cell.index}
-                                        />
-                                    )
-                                }
+              {row.map((cell, idx) => {
+                if (cell.answer === ".") {
+                  return (
+                    <TextInput
+                      maxLength={0}
+                      style={{
+                        backgroundColor: "black",
+                        height: "100%",
+                        width: `${100 / numOfRows}%`,
+                        borderColor: "black",
+                        borderWidth: 1,
+                        justifyContent: "center"
+                      }}
+                      textAlign={"center"}
+                      key={cell.index}
+                    />
+                  );
+                }
 
-                                return (
-                                    <TextInput
-                                        maxLength={1}
-                                        style={{
-                                            backgroundColor: 'white',
-                                            height: '100%',
-                                            width: `${100 / numOfRows}%`,
-                                            borderColor: 'black',
-                                            borderWidth: 1,
-                                            justifyContent: 'center',
-                                        }}
-                                        textAlign={'center'}
-                                        key={cell.index}
-                                        onChangeText={this.handleChange(
-                                            cell.index
-                                        )}
-                                    >
-                                        {cell.guess}
-                                    </TextInput>
-                                )
-                            })}
-                        </View>
-                    )
-                })}
+                return (
+                  <TextInput
+                    maxLength={1}
+                    style={{
+                      backgroundColor: "white",
+                      height: "100%",
+                      width: `${100 / numOfRows}%`,
+                      borderColor: "black",
+                      borderWidth: 1,
+                      justifyContent: "center"
+                    }}
+                    textAlign={"center"}
+                    key={cell.index}
+                    onChangeText={this.handleChange(cell.index)}
+                  >
+                    {cell.guess}
+                  </TextInput>
+                );
+              })}
             </View>
-        )
-    }
+          );
+        })}
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
-    // view: {
-    //   flexDirection: "row",
-    //   height: "25%"
-    // },
-    textinput: {
-        backgroundColor: 'white',
-        height: '100%',
-        // width: `${100 / numOfRows}%`,
-        borderColor: 'black',
-        borderWidth: 2,
-        justifyContent: 'center',
-    },
-    blackSquare: {
-        backgroundColor: 'black',
-        height: '100%',
-        // width: `${100 / numOfRows}%`,
-        borderColor: 'black',
-        borderWidth: 2,
-        justifyContent: 'center',
-    },
-})
+  // view: {
+  //   flexDirection: "row",
+  //   height: "25%"
+  // },
+  textinput: {
+    backgroundColor: "white",
+    height: "100%",
+    // width: `${100 / numOfRows}%`,
+    borderColor: "black",
+    borderWidth: 2,
+    justifyContent: "center"
+  },
+  blackSquare: {
+    backgroundColor: "black",
+    height: "100%",
+    // width: `${100 / numOfRows}%`,
+    borderColor: "black",
+    borderWidth: 2,
+    justifyContent: "center"
+  }
+});
