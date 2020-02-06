f00a8bc3cc91d9aa47e6a2f809de262d7319a8e7
Mike Stanton <mstanton779@gmail.com>
2020-02-06 16:41:28 -0600

    db through sockets, fucked up the beforeValidate

--- server/socket.js
+++ server/socket.js
@@ -1,41 +1,33 @@
 const GameInstance = require("./db/models/gameInstance");
+let roomInfo = {};
 module.exports = io => {
   io.on("connection", socket => {
     // console.log(socket.id, " has made a persistent connection to the server!");
     socket.on("change puzzle", async msg => {
-      // setInterval(async function() {
-      //   console.log("in socket interval");
-      //   console.log("msg.state.guesses[0]: ", msg.state.guesses[0]);
-      //   const gameInstance = await GameInstance.findOne({
-      //     where: {
-      //       id: msg.state.gameId
-      //     }
-      //   });
-      //   const jsonGuesses = msg.state.guesses.map(guess => {
-      //     JSON.stringify(guess);
-      //   });
-      //   const updatedInstance = await gameInstance.update({
-      //     guesses: jsonGuesses
-      //   });
-      //   console.log("updated: ", updatedInstance.guesses[0]);
-      // }, 10000);
       try {
-        console.log("im here");
-        const game = await GameInstance.findOne({
-          where: { id: msg.state.gameId }
-        });
-        console.log("before", msg.state.guesses[0]);
-        // const jsonGuesses = msg.state.guesses.map(guess => {
-        //   return JSON.stringify(guess);
-        // });
-        const jsonGuesses = JSON.stringify(msg.guesses);
-        const updatedGame = await game.update({ guesses: jsonGuesses });
-        console.log("updated game: ", updatedGame.guesses[0]);
+        if (!roomInfo[msg.room]) {
+          roomInfo[msg.room] = { guesses: [], canRequest: true };
+        }
+        roomInfo[msg.room].guesses = msg.guesses;
+        if (roomInfo[msg.room].canRequest === undefined) {
+          roomInfo[msg.room].canRequest = true;
+        }
+        if (roomInfo[msg.room].canRequest) {
+          const game = await GameInstance.findOne({
+            where: { id: msg.room }
+          });
+          const updatedGame = await game.update({
+            guesses: roomInfo[msg.room].guesses
+          });
+          roomInfo[msg.room].canRequest = false;
+
+          setTimeout(function() {
+            roomInfo[msg.room].canRequest = true;
+          }, 1000);
+        }
       } catch (err) {
         console.log(err);
       }
-      // await game.update({ guesses: msg.state.guesses });
-      // socket.on("change puzzle", msg => {
       socket.to(msg.room).emit("change puzzle", msg.guesses);
     });
 
