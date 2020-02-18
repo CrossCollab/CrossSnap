531caada7a86b970a9926e5da3ab18f82c2620a4
Nicky Spiva <nick.spiva@gmail.com>
2020-02-18 11:07:45 -0600

    added comments, some cleanup for CWCell component

--- components/CWCell.js
+++ components/CWCell.js
@@ -3,63 +3,73 @@ import { Text, TextInput, View } from "react-native";
 
 export default function CWCell(props) {
   let cell = props.cell;
-
   let playerColors = props.playerColors;
   let activeCells = props.activeCells;
 
-  let myColor;
+  //The structure of information being pulled in looks like this:
+  // cell = {answer, guess, index, userId}
+  // playerColors = [{color, firstName, userId}, ...]
+  // activeCells = {'userId', index}
 
+  //cellColor represent's the cell's text color, set to the userId who entered the guess' color
+  let cellColor;
+
+  if (cell.index === 10) {
+    console.log("cell = ", cell);
+    console.log("playerColors = ", playerColors);
+    console.log("activeCells = ", activeCells);
+  }
+
+  //if there isn't an array of playerColors (e.g. hasn't yet been pulled in), render
+  //the cell's text in black. Or, if the userId of the cell (default to 0) is not in the
+  //array of playerColors, then display the cell's text in black (should be empty)
   if (
     !playerColors ||
     playerColors.filter(player => player.userId === cell.userId).length === 0
   ) {
-    myColor = "black";
+    cellColor = "black";
   } else {
-    myColor = playerColors.filter(player => player.userId === cell.userId)[0]
+    cellColor = playerColors.filter(player => player.userId === cell.userId)[0]
       .color;
   }
 
   //setting up player active cell status and color
   let cellBackgroundColor;
   let hasBackground = false;
-  let activeCellIndexes = Object.values(activeCells);
 
+  //generates just an array of the active cell indices, e.g. [14, 88, 213]
+  let activeCellIndexes = Object.values(activeCells);
   let activeUserId;
 
+  //set the activeUserId for this cell by seeing if for each key in the activeCells,
+  //if the value (index) matches the index of this particular cell.
   for (const userId in activeCells) {
     if (activeCells[userId] === cell.index) {
       activeUserId = userId;
     }
   }
 
+  //if this cell's index is included in the array of active cell indices, then
+  //figure out the color of the player active in this cell, by matching the activeUserId
+  //from the active cells array, to the userID in the playerColors array
   if (activeCellIndexes.includes(cell.index)) {
-    // console.log("this is an active cell");
-    // console.log("active user id", activeUserId);
-    // console.log("cell index", cell.index);
-    // console.log("player colors", playerColors);
     let indexOfPlayer = playerColors.findIndex(
       element => element.userId == activeUserId
     );
-    // console.log("index of player in pcolor array", indexOfPlayer);
     let playerColor;
     if (indexOfPlayer < 0) {
       playerColor = "green";
     } else {
       playerColor = playerColors[indexOfPlayer].color;
     }
-    // cellBackgroundColor = playerColors.filter(
-    //   player => player.userId === activeUserId
-    // )[0].color;
     cellBackgroundColor = playerColor;
-    // cellBackgroundColor = playerColors.filter(
-    //   player => player.userId === activeCells[cell.index]
-    // )[0].color;
     hasBackground = true;
   }
 
   //if the cell is a black cell, e.g. has no answer/input area
   if (cell.answer === ".") {
     return (
+      //hide the caret, make it gray
       <TextInput
         caretHidden={true}
         key={cell.index}
@@ -71,6 +81,10 @@ export default function CWCell(props) {
           width: `${100 / props.rowCount}%`,
           justifyContent: "center"
         }}
+        //if this black cell gets focused on, just pass the focus on appropriately
+        //depends on view (across/down) and direction of movement (previous/next)
+        //direction is set when a key is pressed. If delete, set to backwards, else fwd.
+        //this is set in CrosswordScreen
         onFocus={event => {
           if (props.currentView === "across") {
             if (props.direction === "forward") {
@@ -115,15 +129,19 @@ export default function CWCell(props) {
         }}
       ></TextInput>
     );
-    //else if the cell has some stored answer value
+    //else if the cell has some stored answer value (ISNT BLACK CELL)
   } else {
     return (
       <View
         key={cell.index}
         onPress={() => {
-          props.refs[cell.index].current.focus();
           //focus on the text input when the touchable opacity is pressed
+          props.refs[cell.index].current.focus();
         }}
+        //For the wrapping view's background color
+        //if this is the current cell the user if focused on, make it yellow
+        //else if this is a cell with the same across or down clue, then color
+        //it light blue to show the active word on the board
         style={{
           backgroundColor:
             cell.index === props.currentCell.index
@@ -171,9 +189,12 @@ export default function CWCell(props) {
           ></Text>
         )}
         <TextInput
-          // blurOnSubmit={false}
           autoCapitalize="characters"
           maxLength={1}
+          //if, earlier, we determined that a user was active in this cell,
+          //give this cell the light background color of that user
+          //else if the game board has been checked and the cell was found correct
+          //give the cell the green correct background, else white background
           style={{
             backgroundColor: hasBackground
               ? `light${cellBackgroundColor}`
@@ -187,9 +208,8 @@ export default function CWCell(props) {
             zIndex: 9999,
             top: "12%",
             fontSize: 8,
-
-            color: myColor ? myColor : "black",
-
+            //text appears in as the color of the user who entered the guess, or black (which should only be for empties..)
+            color: cellColor ? cellColor : "black",
             fontWeight: "bold"
           }}
           ref={props.refs[cell.index]}
